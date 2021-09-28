b13359fd06575d471fb9b29ecfa0a56a5ae9002c
johndward01 <johndward01@gmail.com>
2021-09-28 12:00:04 -0500

    added Create and Index Views

---
+++ ASP.NET Core Identity Demo/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using ASP.NET_Core_Identity_Demo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Identity_Demo.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        private IPasswordHasher<AppUser> passwordHasher;
+
+        public AdminController(UserManager<AppUser> usrMgr, IPasswordHasher<AppUser> passwordHash)
+        {
+            userManager = usrMgr;
+        }
+
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    UserName = user.Name,
+                    Email = user.Email
+                };
+
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
+        }
+    }
+}
