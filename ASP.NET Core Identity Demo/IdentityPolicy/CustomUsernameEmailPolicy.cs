51e96a5c5db6911cb69e2f92a1c50c7e9e77a0ea
johndward01 <johndward01@gmail.com>
2021-09-30 13:24:19 -0500

    update

--- ASP.NET Core Identity Demo/IdentityPolicy/CustomUsernameEmailPolicy.cs
+++ ASP.NET Core Identity Demo/IdentityPolicy/CustomUsernameEmailPolicy.cs
@@ -1,11 +1,8 @@
 ﻿using ASP.NET_Core_Identity_Demo.Models;
 using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ASP.NET_Core_Identity_Demo.Models;
-using Microsoft.AspNetCore.Identity;
 
 namespace ASP.NET_Core_Identity_Demo.IdentityPolicy
 {
