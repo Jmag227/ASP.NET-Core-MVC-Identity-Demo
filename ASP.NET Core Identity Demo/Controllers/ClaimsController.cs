00bada9a5c0e68c8984215f68279eb84378a6e6a
jdw <johndward01@gmail.com>
2021-10-04 18:35:55 -0500

    * Stopping point: https://www.yogihosting.com/aspnet-core-identity-policies/

--- ASP.NET Core Identity Demo/Controllers/ClaimsController.cs
+++ ASP.NET Core Identity Demo/Controllers/ClaimsController.cs
@@ -1,16 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.NET_Core_Identity_Demo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ASP.NET_Core_Identity_Demo.Controllers
 {
     public class ClaimsController : Controller
     {
-        public IActionResult Index()
+
+        private UserManager<AppUser> userManager;
+
+        public ClaimsController(UserManager<AppUser> userMgr)
         {
+            userManager = userMgr;
+        }
+
+        public ViewResult Index() => View(User?.Claims);
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        [ActionName("Create")]
+        public async Task<IActionResult> Create_Post(string claimType, string claimValue)
+        {
+            AppUser user = await userManager.GetUserAsync(HttpContext.User);
+            Claim claim = new Claim(claimType, claimValue, ClaimValueTypes.String);
+            IdentityResult result = await userManager.AddClaimAsync(user, claim);
+
+            if (result.Succeeded)
+                return RedirectToAction("Index");
+            else
+                Errors(result);
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string claimValues)
+        {
+            AppUser user = await userManager.GetUserAsync(HttpContext.User);
+
+            string[] claimValuesArray = claimValues.Split(";");
+            string claimType = claimValuesArray[0], claimValue = claimValuesArray[1], claimIssuer = claimValuesArray[2];
+
+            Claim claim = User.Claims.Where(x => x.Type == claimType && x.Value == claimValue && x.Issuer == claimIssuer).FirstOrDefault();
+
+            IdentityResult result = await userManager.RemoveClaimAsync(user, claim);
+
+            if (result.Succeeded)
+                return RedirectToAction("Index");
+            else
+                Errors(result);
+
+            return View("Index");
+        }
+
+        void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
         }
     }
 }
